93639517f41630961a2b46a19a1467475a93367d
Austin Stotts <astotts@cdicon.com>
2023-09-11 10:43:03 -0500

    added an info page... deploying v1.0.1

--- src/search.js
+++ src/search.js
@@ -8,12 +8,15 @@ export default class Search extends React.Component {
         super()
 
         this.state = {
-            playerid: ''
+            playerid: '',
+            info: false,
+            blur: false,
         }
 
         this.textChange = this.textChange.bind(this);
         this.searchPlayer = this.searchPlayer.bind(this);
         this.isEnter = this.isEnter.bind(this);
+        this.showInfo = this.showInfo.bind(this);
     }
 
     isEnter (event) {
@@ -31,10 +34,24 @@ export default class Search extends React.Component {
         window.location = `/player/${this.state.playerid}`
     }
 
+    showInfo () {
+        this.setState({ info: !this.state.info, blur: !this.state.blur })
+    }
+
     render () {
         return (
             <div className="search-wrapper">
-                <div className="search-box">
+                <dialog className="info-wrapper" open={this.state.info}>
+                    <div className="info-item">hello!</div>
+                    <div className="info-item">my name is austin stotts (steve)</div>
+                    <div className="info-item">this is a personal project for my own use</div>
+                    <div className="info-item">if you have any issues / concerns</div>
+                    <div className="info-item">send an email to <span className="mail">stevelovescartoons@gmail.com</span></div>
+                    <div className="info-item">v1.0.1</div>
+                    <div className="close" onClick={this.showInfo}>close</div>
+                </dialog>
+                <div className="slow" onClick={this.showInfo}><span className="l-s b">S</span>teve<span className="l-l b">L</span>oves<span className="l-ow b">OW</span></div>
+                <div className={`search-box ${this.state.blur ? "blur" : ""}`}>
                     <input onKeyDown={this.isEnter} className="search-input" autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" type="textbox" placeholder="steve#19145" onChange={this.textChange}></input>
                     <button className="search-button" onClick={this.searchPlayer}>search</button>
                 </div>
